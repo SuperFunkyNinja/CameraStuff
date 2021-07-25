b876a76b0160520becbd1cd5e37b5620471f3be9
SuperFunkyNinja <superfunkyninja@gmail.com>
2021-07-25 18:49:10 +0100

    Added text overlay

--- CameraScratch.py
+++ CameraScratch.py
@@ -5,6 +5,8 @@ camera = PiCamera()
 camera.rotation = 180
 
 camera.start_preview()
+camera.annotate_text = "Hello Marty!"
+camera.start_recording('/home/pi/Desktop/video.h264')
 sleep(5)
 camera.capture('/home/pi/Desktop/image.jpg')
 camera.stop_preview()
